d205eeb4ffe5eff07ffad5c06adfdffa14fe8e0b
Rajdeep Chakravorty <98254554+RajDeep-Chakravorty@users.noreply.github.com>
2024-04-25 13:03:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,14 @@ def set_bg_hack_url():
 # Call the function to set background image
 set_bg_hack_url()
 
+# Set page config
+st.set_page_config(
+    page_title="Olympics Data Analysis App",
+    page_icon="https://cdn.britannica.com/01/23901-050-33507FA4/flag-Olympic-Games.jpg",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
 
 df = pd.read_csv('athlete_events.csv')
 region = pd.read_csv('noc_regions.csv')
